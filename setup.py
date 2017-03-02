eb92e1cbc48d8d906114177ed9b79a6add9448a8
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-03-02 23:47:48 +0100

    Set version to 0.3.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='cointrader',
-    version='0.2.0',
+    version='0.3.0',
     description="Cointrader is a trading application for crypto currencies.",
     long_description=readme + '\n\n' + history,
     author="Torsten Irländer",
