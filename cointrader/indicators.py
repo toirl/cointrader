667211943a45dc1ab617bc7399334231d9ef01d0
Torsten Irländer <torsten.irlaender@googlemail.com>
2017-03-27 14:10:14 +0200

    Make tests on followtrend work again.

--- cointrader/indicators.py
+++ cointrader/indicators.py
@@ -66,74 +66,74 @@ def is_min_value(values):
 #     return Signal(signal, datetime.datetime.utcfromtimestamp(d))
 #
 #
-# def followtrend(data, sluggish=1.5):
-#     support = None
-#     resistance = None
-#     last = data[0][1]
-#     signal = WAIT
-#
-#     def breaks_resistance(v, resistance, sluggish):
-#         resistance = resistance + (resistance / 100 * sluggish)
-#         return v > resistance
-#
-#     def breaks_support(v, support, sluggish):
-#         support = support - (support / 100 * sluggish)
-#         return v < support
-#
-#     for item in data:
-#         d = item[0]
-#         v = item[1]
-#         if resistance is None:
-#             # We are searching a new resistance and wait for a change in
-#             # the current trend.
-#             if v < last:
-#                 resistance = last
-#         if support is None:
-#             # We are searching a new support and wait for a change in
-#             # the current trend.
-#             if v > last:
-#                 support = last
-#         if resistance is not None and support is not None:
-#             # Trend is now in correction phase. It s changing with in
-#             # the range of the last resistance and last support.
-#             if not breaks_resistance(v, resistance, sluggish) and not breaks_support(v, support, sluggish):
-#                 # Nothing special here no signal.
-#                 signal = WAIT
-#             elif breaks_resistance(v, resistance, sluggish):
-#                 # Trend breaks the last resistance. This is a BUY
-#                 # signal. The resistiance is gone and the support will
-#                 # be the last resistance.
-#
-#                 #  TODO: Better check the effect of using
-#                 #  last/resistance as the new support. Currently I
-#                 #  suspect that using the old resitiance as the new
-#                 #  support is more relaxed and leaves a larger
-#                 #  correction band and not triggering false SELL
-#                 #  signales. Using last seems to be more greedy
-#                 #  but results in small correction bands and potentially
-#                 #  more wrong sell signal. (ti) <2017-02-24 21:37>
-#                 support = resistance
-#                 # support = last
-#
-#                 resistance = None
-#                 signal = BUY
-#             elif breaks_support(v, support, sluggish):
-#                 # Trend breaks the last support. This is a SELL
-#                 # signal. The support is gone and the resistance will
-#                 # be the last support.
-#
-#                 #  TODO: Better check the effect of using
-#                 #  last/support as the new resistance. See above TODO
-#                 #  for more comments. (ti) <2017-02-24 21:37>
-#                 resistance = support
-#                 # resistance = last
-#
-#                 support = None
-#                 signal = SELL
-#         last = v
-#         log.debug("{} signal @ {}: Value: {}, Resistance: {}, Support: {}".format(signal_map[signal],
-#                                                                                   datetime.datetime.utcfromtimestamp(d),
-#                                                                                   v,
-#                                                                                   resistance,
-#                                                                                   support))
-#     return Signal(signal, datetime.datetime.utcfromtimestamp(d))
+def followtrend(data, sluggish=1.5):
+    support = None
+    resistance = None
+    last = data[0][1]
+    signal = WAIT
+
+    def breaks_resistance(v, resistance, sluggish):
+        resistance = resistance + (resistance / 100 * sluggish)
+        return v > resistance
+
+    def breaks_support(v, support, sluggish):
+        support = support - (support / 100 * sluggish)
+        return v < support
+
+    for item in data:
+        d = item[0]
+        v = item[1]
+        if resistance is None:
+            # We are searching a new resistance and wait for a change in
+            # the current trend.
+            if v < last:
+                resistance = last
+        if support is None:
+            # We are searching a new support and wait for a change in
+            # the current trend.
+            if v > last:
+                support = last
+        if resistance is not None and support is not None:
+            # Trend is now in correction phase. It s changing with in
+            # the range of the last resistance and last support.
+            if not breaks_resistance(v, resistance, sluggish) and not breaks_support(v, support, sluggish):
+                # Nothing special here no signal.
+                signal = WAIT
+            elif breaks_resistance(v, resistance, sluggish):
+                # Trend breaks the last resistance. This is a BUY
+                # signal. The resistiance is gone and the support will
+                # be the last resistance.
+
+                #  TODO: Better check the effect of using
+                #  last/resistance as the new support. Currently I
+                #  suspect that using the old resitiance as the new
+                #  support is more relaxed and leaves a larger
+                #  correction band and not triggering false SELL
+                #  signales. Using last seems to be more greedy
+                #  but results in small correction bands and potentially
+                #  more wrong sell signal. (ti) <2017-02-24 21:37>
+                support = resistance
+                # support = last
+
+                resistance = None
+                signal = BUY
+            elif breaks_support(v, support, sluggish):
+                # Trend breaks the last support. This is a SELL
+                # signal. The support is gone and the resistance will
+                # be the last support.
+
+                #  TODO: Better check the effect of using
+                #  last/support as the new resistance. See above TODO
+                #  for more comments. (ti) <2017-02-24 21:37>
+                resistance = support
+                # resistance = last
+
+                support = None
+                signal = SELL
+        last = v
+        log.debug("{} signal @ {}: Value: {}, Resistance: {}, Support: {}".format(signal_map[signal],
+                                                                                  datetime.datetime.utcfromtimestamp(d),
+                                                                                  v,
+                                                                                  resistance,
+                                                                                  support))
+    return Signal(signal, datetime.datetime.utcfromtimestamp(d))
--- tests/strategies/test_followtrend.py
+++ tests/strategies/test_followtrend.py
@@ -5,14 +5,14 @@
 test_followtrend
 ----------------------------------
 
-Tests for `cointrader.strategies.trend` module.
+Tests for `cointrader.indicators` module.
 """
 
 
 def test_monoton_raising():
     """Monoton raising chart. State is still in in initial phase.
     Therefor no signal is emitted"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart1 = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9)]
     signal = followtrend(chart1)
     assert signal.value == 0
@@ -21,7 +21,7 @@ def test_monoton_raising():
 def test_monoton_falling():
     """Monoton falling chart.  State is still in in initial phase.
     Therefor no signal is emitted"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart1 = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9)]
     chart1.reverse()
     signal = followtrend(chart1)
@@ -32,7 +32,7 @@ def test_localmin_found():
     """Test initial phase. After reaching a local minimum no local
     maximum has been found yet. No signal is emitted. We are still
     waiting for entering the correction phase"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 9), (0, 8), (0, 5), (0, 4), (0, 3), (0, 2), (0, 1), (0, 2), (0, 4), (0, 5)]
     signal = followtrend(chart)
     assert signal.value == 0
@@ -42,7 +42,7 @@ def test_localmax_found():
     """Test initial phase. After reaching a local maximum no local
     minimum has been found yet. No signal is emitted. We are still
     waiting for entering the correction phase"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 1), (0, 2), (0, 5), (0, 7), (0, 8), (0, 7), (0, 6), (0, 5), (0, 4)]
     signal = followtrend(chart)
     assert signal.value == 0
@@ -52,7 +52,7 @@ def test_raising_correction():
     """Test correction phase. After reaching a local maximum and local
     minimum we are in correction phase. No signal is emitted. We are still
     waiting for crossing the last maximum."""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 1), (0, 2), (0, 5), (0, 4), (0, 3), (0, 4), (0, 3), (0, 4), (0, 3)]
     signal = followtrend(chart)
     assert signal.value == 0
@@ -62,7 +62,7 @@ def test_falling_correction():
     """Test correction phase. After reaching a local maximum and local
     minimum we are in correction phase. No signal is emitted. We are still
     waiting for crossing the last minimum."""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 9), (0, 7), (0, 5), (0, 1), (0, 2), (0, 3), (0, 6), (0, 2), (0, 3), (0, 2), (0, 3)]
     signal = followtrend(chart)
     assert signal.value == 0
@@ -72,7 +72,7 @@ def test_raising_buy_signal():
     """Test correction phase. After reaching a local maximum and local
     minimum we are in correction phase and the last maximum is exeeded.
     BUY signal is emitted"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 1), (0, 2), (0, 5), (0, 4), (0, 3), (0, 4), (0, 3), (0, 4), (0, 3), (0, 5), (0, 6)]
     signal = followtrend(chart)
     assert signal.value == 1
@@ -82,7 +82,7 @@ def test_raising_sell_signal():
     """Test correction phase. After reaching a local maximum and local
     minimum we are in correction phase and the last minimum is exeeded.
     SELL signal is emitted"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 7), (0, 3), (0, 5), (0, 4), (0, 3), (0, 4), (0, 3), (0, 4), (0, 3), (0, 3), (0, 2)]
     signal = followtrend(chart)
     assert signal.value == -1
@@ -92,7 +92,7 @@ def test_falling_buy_signal():
     """Test correction phase. After reaching a local maximum and local
     minimum we are in correction phase and the last maximum is exeeded.
     BUY signal is emitted"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 9), (0, 8), (0, 5), (0, 6), (0, 7), (0, 6), (0, 5), (0, 6), (0, 1), (0, 0)]
     signal = followtrend(chart)
     assert signal.value == -1
@@ -102,7 +102,7 @@ def test_falling_sell_signal():
     """Test correction phase. After reaching a local maximum and local
     minimum we are in correction phase and the last maximum is exeeded.
     BUY signal is emitted"""
-    from cointrader.strategies.trend import followtrend
+    from cointrader.indicators import followtrend
     chart = [(0, 9), (0, 8), (0, 5), (0, 6), (0, 7), (0, 6), (0, 5), (0, 5), (0, 4)]
     signal = followtrend(chart)
     assert signal.value == -1
